c22a527dbd2eacdb5cc427d6aebca25e76e6a8b4
chaeyi0318 <chaeyi0318@gmail.com>
2024-11-18 17:33:56 +0900

    feat: 회원가입, 로그인 기능 추가

--- final_pjt_back/accounts/models.py
+++ final_pjt_back/accounts/models.py
@@ -1,6 +1,14 @@
+# accounts/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=20, unique=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    nickname = models.CharField(max_length=30, unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    income = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    assets = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    point = models.IntegerField(default=0)
