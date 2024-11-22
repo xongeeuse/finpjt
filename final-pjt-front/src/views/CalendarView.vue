ab7f32b1bc14137935c5063fbfea359578fea8be
chaeyi0318 <chaeyi0318@gmail.com>
2024-11-22 17:50:42 +0900

    Fix: 비율 수정

--- final-pjt-front/src/views/CalendarView.vue
+++ final-pjt-front/src/views/CalendarView.vue
@@ -9,7 +9,10 @@
       </form>
       <span>{{ cal.monthText }}월 총 소비 금액 : {{ total_price }}</span>
       <div v-for="categoryValue in categorySumValue" :key="categoryValue.id">
-        <p>{{ categoryValue.category_name }} : {{ categoryValue.total_price }} 비율 : {{ (Math.round((categoryValue.total_price / total_price) * 100 * 100) / 100).toFixed(2) }}%</p>
+        <p>{{ categoryValue.category_name }} : {{ categoryValue.total_price }}
+          <br>
+          비율 : {{ total_price === 0 ? 0  : (Math.round((categoryValue.total_price / total_price) * 100 * 100) / 100).toFixed(2)}}%
+        </p>
       </div>
 
 
