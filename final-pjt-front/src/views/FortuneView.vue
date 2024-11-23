ee9c9c2b513077f116855f2d40a999e946476e24
xongeeuse <xongeeuse@gmail.com>
2024-11-23 22:12:04 +0900

    Feat: 소비 카테고리 그래프

--- final-pjt-front/src/views/FortuneView.vue
+++ final-pjt-front/src/views/FortuneView.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>운세</h1>
+    <CategoryChart />
   </div>
 </template>
 
 <script setup>
-
+import CategoryChart from '@/components/calendar/CategoryChart.vue';
 </script>
 
 <style lang="scss" scoped>
